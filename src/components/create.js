2453123fc124203d85ed94a99d65ab11a4fba0d4
momin-riyadh <mominriyadh@gmail.com>
2021-08-22 13:28:49 +0600

    fix and deploy

--- src/components/create.js
+++ src/components/create.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Checkbox, Form} from 'semantic-ui-react'
 
-const Create = () => (
-    <Form className="create-form">
+function Create() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false)
+
+    return <Form className="create-form">
         <Form.Field>
             <label>First Name</label>
-            <input placeholder='First Name'/>
+            <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
         </Form.Field>
         <Form.Field>
             <label>Last Name</label>
-            <input placeholder='Last Name'/>
+            <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
         </Form.Field>
         <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions'/>
+            <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
         </Form.Field>
         <Button type='submit'>Submit</Button>
-    </Form>
-)
+    </Form>;
+}
 
 export default Create;
