48b514dd7539591c1ca3f11006bdbd7eb054b907
momin-riyadh <mominriyadh@gmail.com>
2021-08-22 13:03:36 +0600

    fix and deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <div className="main">
-        React Crud Operations
+          <h2 className="main-header">
+              React Crud Operations
+          </h2>
       </div>
     </div>
   );
