f159d459b0f4b235e768f1bc5b1856de1cbbe841
momin-riyadh <mominriyadh@gmail.com>
2021-08-22 17:10:32 +0600

    fix and deploy

--- src/components/read.js
+++ src/components/read.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Table} from 'semantic-ui-react';
+import axios from "axios";
 
 export default function Read() {
+
+    const [APIData, setAPIData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://6121f7ecf5849d0017fb4334.mockapi.io/fakedata`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
     return (
         <div>
             <Table singleLine>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Registration Date</Table.HeaderCell>
-                        <Table.HeaderCell>E-mail address</Table.HeaderCell>
-                        <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                        <Table.HeaderCell>First Name</Table.HeaderCell>
+                        <Table.HeaderCell>Last Name</Table.HeaderCell>
+                        <Table.HeaderCell>Checked</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    <Table.Row>
-                        <Table.Cell>John Lilki</Table.Cell>
-                        <Table.Cell>September 14, 2013</Table.Cell>
-                        <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-                        <Table.Cell>No</Table.Cell>
-                    </Table.Row>
+                    {APIData.map((data) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell>{data.firstName}</Table.Cell>
+                                <Table.Cell>{data.lastName}</Table.Cell>
+                                <Table.Cell>{data.checkbox ? 'checked' : 'unchecked'}</Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+
                 </Table.Body>
             </Table>
         </div>
