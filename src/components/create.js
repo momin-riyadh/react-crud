d93747e6e2d4907b172fae2635e53bfc607d928c
momin-riyadh <mominriyadh@gmail.com>
2021-08-22 19:37:54 +0600

    fix and deploy

--- src/components/create.js
+++ src/components/create.js
@@ -6,39 +6,50 @@ import axios from "axios";
 function Create() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [checkbox, setCheckbox] = useState(false)
-    const [emailAddress, setEmailAddress] = useState('')
+    const [checkbox, setCheckbox] = useState(false);
+    const [emailAddress, setEmailAddress] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
 
-    const postData = () => {
-        axios.post(`https://6121f7ecf5849d0017fb4334.mockapi.io/fakedata`, {
+    const postData = async () => {
+
+        await axios.post(`https://6121f7ecf5849d0017fb4334.mockapi.io/fakedata`, {
             firstName,
             lastName,
             emailAddress,
             checkbox
         })
+        setIsSubmitted(true);
     }
 
-    return <Form className="create-form">
-        <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
-        </Form.Field>
-        <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
-        </Form.Field>
-        <Form.Field>
-            <label>Email</label>
-            <input placeholder='Your Email' onChange={(e) => setEmailAddress(e.target.value)}/>
-        </Form.Field>
-        <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
-        </Form.Field>
-        <Button onClick={postData} type='submit'>Submit</Button>
 
-    </Form>;
+    if (!isSubmitted) {
+        return <Form className="create-form">
+            <Form.Field>
+                <label>First Name</label>
+                <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <label>Last Name</label>
+                <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <label>Email</label>
+                <input placeholder='Your Email' onChange={(e) => setEmailAddress(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
+            </Form.Field>
+            <Button onClick={postData} type='submit'>Submit</Button>
 
+        </Form>;
+
+    }
+    else {
+        return (
+            <div id="confirmation">Your form has been submitted</div>
+        )
+
+    }
 }
-
 export default Create;
