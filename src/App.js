054af2656f9ee36aeceb2d6c680988088561490f
momin-riyadh <mominriyadh@gmail.com>
2021-08-22 13:20:00 +0600

    fix and deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import Create from "./components/create";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="main">
-          <h2 className="main-header">
-              React Crud Operations
-          </h2>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="main">
+                <h2 className="main-header">
+                    React Crud Operations
+                </h2>
+                <div>
+                    <Create/>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
